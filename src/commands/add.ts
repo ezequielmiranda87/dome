f87b3c710b7e56d6c95514de26fd61664ff61f1b
ezexiel <ezequielmiranda7@gmail.com>
2020-03-03 08:27:32 +0100

    ok

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,25 +1,23 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
+const chalk = require('chalk')
 
 export default class Add extends Command {
-  static description = 'describe the command here'
+  static description = 'Add new task to the list'
 
-  static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+  static flags = {};
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "todo" }]
 
   async run() {
-    const {args, flags} = this.parse(Add)
+    const { args, flags } = this.parse(Add)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/ezequielmiranda/server/node_cli/dome/src/commands/add.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const todo = args.todo
+
+    if (todo) {
+      this.log(` ${chalk.green(['Success'])} added new task - ${todo}`)
+    } else {
+      this.error('Specify the new todo')
+      this.log(` ${chalk.yellow(['Error'])} - the task couldn't be added`)
     }
   }
 }
